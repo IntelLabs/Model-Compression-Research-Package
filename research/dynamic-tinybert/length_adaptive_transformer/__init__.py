73e5b93e2d6f1da3e4b4d94f6fbb8d513bfee650
Shira <shira.guskin@intel.com>
2021-12-12 23:14:52 +0200

    rename and add license

---
+++ research/dynamic-tinybert/length_adaptive_transformer/__init__.py
@@ -0,0 +1,53 @@
+# Apache v2 license
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+# 
+# This file is copied from https://github.com/clovaai/length-adaptive-transformer
+# with roberta model added
+# Length-Adaptive Transformer
+# Copyright (c) 2020-present NAVER Corp.
+# Apache License v2.0
+
+from transformers import (
+    MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING,
+    MODEL_FOR_QUESTION_ANSWERING_MAPPING,
+    BertConfig,
+    DistilBertConfig,
+    RobertaConfig,
+)
+
+from .modeling_bert import (
+    BertForSequenceClassification,
+    BertForQuestionAnswering,
+)
+from .modeling_distilbert import (
+    DistilBertForSequenceClassification,
+    DistilBertForQuestionAnswering,
+)
+
+from .modeling_roberta import (
+    RobertaForSequenceClassification,
+    RobertaForQuestionAnswering,
+)
+
+
+MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING.update(
+    [
+        (BertConfig, BertForSequenceClassification),
+        (DistilBertConfig, DistilBertForSequenceClassification),
+        (RobertaConfig, RobertaForSequenceClassification),
+    ]
+)
+
+MODEL_FOR_QUESTION_ANSWERING_MAPPING.update(
+    [
+        (BertConfig, BertForQuestionAnswering),
+        (DistilBertConfig, DistilBertForQuestionAnswering),
+        (RobertaConfig, RobertaForQuestionAnswering),
+    ]
+)
+
+
+from .training_args import TrainingArguments
+from .drop_and_restore_utils import LengthDropArguments, SearchArguments
+from .trainer import LengthDropTrainer
