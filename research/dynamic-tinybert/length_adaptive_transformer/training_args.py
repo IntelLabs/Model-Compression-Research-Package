1bc5042d4a6372b5ff29b544ddb29fd45d4bc208
Shira <shira.guskin@intel.com>
2021-12-13 11:06:51 +0200

    add requirements

--- research/dynamic-tinybert/length_adaptive_transformer/training_args.py
+++ research/dynamic-tinybert/length_adaptive_transformer/training_args.py
@@ -1,3 +1,10 @@
+# Apache v2 license
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+# 
+# This file is copied from https://github.com/clovaai/length-adaptive-transformer
+
+
 # Length-Adaptive Transformer
 # Copyright (c) 2020-present NAVER Corp.
 # Apache License v2.0
